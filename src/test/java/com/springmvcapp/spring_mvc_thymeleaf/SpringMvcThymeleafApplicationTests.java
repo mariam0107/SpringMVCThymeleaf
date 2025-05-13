6d374c55d5d8333b40b13e669de898867b4efc11
mariam george <mariam0107@gmail.com>
2025-05-13 07:05:23 +0200

    Created Spring MVC Thyme leaf project

---
+++ src/test/java/com/springmvcapp/spring_mvc_thymeleaf/SpringMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvcapp.spring_mvc_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
